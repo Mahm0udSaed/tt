3c3b448e69cb304982d1fc028d14e2b24a524099
Mahm0udSaed <mahmoudsaltah145@gmail.com>
2020-12-20 13:35:59 +0200

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -5,4 +5,18 @@ multibranchPipelineJob('configuration-as-code-via-url') {
           remote('https://github.com/mkrzyzanowski/blog-001.git')
       }
   }
+  agent { label 'master' }
+
+    stages {
+	stage('Build') {
+            steps {
+                sh 'mvn install'
+            }
+        }
+        
+	
+    }
+
+ 
+
 }
