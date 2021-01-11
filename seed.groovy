ccbd51bb2e6bc7fafc63b4e40834578d80f4d9bc
Mahm0udSaed <mahmoudsaltah145@gmail.com>
2021-01-11 22:32:56 +0200

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -4,6 +4,8 @@ multibranchPipelineJob('configuration-as-code-via-url') {
       source {
               gitSCMSource {
                 remote('git@gitlab.com:Mahmoud_Saeed/jenkins-integration.git')
+                credentialsId('736dbbbb-1559-41f1-bcf6-c2338629746f')
+                
               
               }
               
